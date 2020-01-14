0f3d74595b6dc9c4b5b863ebe706a8ebcde48604
MacBook <macbook@MacBook-Pro-MacBook.local>
2020-01-14 16:25:14 +0300

    start

--- App.js
+++ App.js
@@ -1,12 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -15,59 +7,55 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
 import DateTimePicker from '@react-native-community/datetimepicker';
 
-const App: () => React$Node = () => {
-  const date = new Date('2020-06-12T14:42:42');
-  const mode = 'date';
-  const show = true;
+const App = () => {
+  const [fromDate, setFromDate] = useState(new Date());
+  const [toDate, setToDate] = useState(new Date());
+
+  const [show, setShow] = useState(true);
+
+  const updateFromDate = (event, date) => {
+    setFromDate(date);
+  };
+
+  const updateToDate = (event, date) => {
+    setToDate(date);
+  };
+
+  const calcDate = () => {
+    const dateNow = new Date().setHours(0);
+    const myDate = fromDate - dateNow;
+    return myDate;
+  };
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
+              <Text>Start</Text>
               <DateTimePicker
-                value={date}
-                mode={mode}
-                is24Hour={true}
+                value={fromDate}
                 display="default"
-                onChange={this.setDate}
+                onChange={updateFromDate}
               />
             </View>
-            <View style={styles.sectionContainer}></View>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
+              <Text>To</Text>
+              <DateTimePicker
+                value={toDate}
+                display="default"
+                onChange={updateToDate}
+              />
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text>
+                this date is {Math.ceil(calcDate() / (1000 * 3600 * 24))}
               </Text>
             </View>
-            <LearnMoreLinks />
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -76,41 +64,9 @@ const App: () => React$Node = () => {
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
   },
 });
 
